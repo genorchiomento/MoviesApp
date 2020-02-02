d2223e4138157418ecbbae48415c470ecf101b14
Genor Chiomento <genor.chiomento@gmail.com>
2020-02-02 19:43:38 -0300

    Adding retrofit config and removing base activity

---
+++ app/src/main/java/com/arctouch/codechallenge/api/RetrofitConfig.kt
@@ -0,0 +1,32 @@
+package com.arctouch.codechallenge.api
+
+import com.arctouch.codechallenge.util.Constants
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+class RetrofitConfig {
+
+  private val client by lazy {
+    val interceptor = HttpLoggingInterceptor()
+    interceptor.level = HttpLoggingInterceptor.Level.BODY
+    OkHttpClient.Builder()
+        .addInterceptor(interceptor)
+        .build()
+  }
+
+  private val retrofit by lazy {
+    Retrofit.Builder()
+        .baseUrl(Constants.URL)
+        .client(client)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .build()
+  }
+
+  val apiService: TmdbApi by lazy {
+    retrofit.create(TmdbApi::class.java)
+  }
+}
--- app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
+++ app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
@@ -1,34 +1,38 @@
 package com.arctouch.codechallenge.home
 
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.arctouch.codechallenge.R
+import com.arctouch.codechallenge.api.RetrofitConfig
 import com.arctouch.codechallenge.api.TmdbApi
-import com.arctouch.codechallenge.base.BaseActivity
 import com.arctouch.codechallenge.data.Cache
+import com.arctouch.codechallenge.util.Constants
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.home_activity.*
 
-class HomeActivity : BaseActivity() {
+class HomeActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.home_activity)
+  private val service: TmdbApi = RetrofitConfig().apiService
 
-        api.upcomingMovies(
-            TmdbApi.API_KEY,
-            TmdbApi.DEFAULT_LANGUAGE,
-            1,
-            TmdbApi.DEFAULT_REGION)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe {
-                val moviesWithGenres = it.results.map { movie ->
-                    movie.copy(genres = Cache.genres.filter { movie.genreIds?.contains(it.id) == true })
-                }
-                recyclerView.adapter = HomeAdapter(moviesWithGenres)
-                progressBar.visibility = View.GONE
-            }
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.home_activity)
+
+    service.upcomingMovies(
+        Constants.API_KEY,
+        Constants.DEFAULT_LANGUAGE,
+        1,
+        Constants.DEFAULT_REGION)
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe {
+          val moviesWithGenres = it.results.map { movie ->
+            movie.copy(genres = Cache.genres.filter { movie.genreIds?.contains(it.id) == true })
+          }
+          recyclerView.adapter = HomeAdapter(moviesWithGenres)
+          progressBar.visibility = View.GONE
+        }
+  }
 }
